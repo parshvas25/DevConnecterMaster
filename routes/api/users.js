4bb49e1a74a4015d6094dca0964960616ddb8462
Parshva Shah <parshvashah@Parshvas-MBP.hitronhub.home>
2019-08-05 10:52:01 -0400

    Added User Post Request and authentication

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router = express.Router();
-
-//@route    GET api/users
-//@desc     Test route
+const { check, validationResult } = require('express-validator');
+//@route    POST api/users
+//@desc     Register User
 //@access   Public
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password that is 6 or more characters long'
+    ).isLength({
+      min: 6
+    })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('User route');
+  }
+);
 
 module.exports = router;
